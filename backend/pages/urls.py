d7843e8c7cb49c66fb421bd111c5c87a6c3b0ec8
Jerin Peter George <jerinpetergeorge@gmail.com>
2021-06-26 19:37:16 +0530

    basic react-django connection established

--- backend/pages/urls.py
+++ backend/pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, user_list
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('users/', user_list, name="user-list")
 ]
--- backend/pages/views.py
+++ backend/pages/views.py
@@ -1,3 +1,5 @@
+import requests
+from django.http import JsonResponse
 from django.views.generic import TemplateView
 
 
@@ -7,3 +9,9 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
+
+def user_list(request):
+    url = "https://jsonplaceholder.typicode.com/users"
+    response = requests.get(url=url)
+    return JsonResponse(data=response.json(), safe=False)
